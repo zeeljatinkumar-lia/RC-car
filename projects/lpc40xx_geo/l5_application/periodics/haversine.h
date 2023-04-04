32595830ca891f9cf4c8dea1975a83c55f25011e
Rashmi Vaidya <rashmisuhas.vaidya@sjsu.edu>
2023-04-04 10:11:08 -0700

    added haversine formula with tests to geo controller

---
+++ projects/lpc40xx_geo/l5_application/periodics/haversine.h
@@ -0,0 +1,4 @@
+#pragma once
+
+double calculate_heading(double lat1, double lon1, double lat2, double lon2);
+double calculate_distance(double lat1, double lon1, double lat2, double lon2);
--- projects/lpc40xx_geo/l5_application/periodics/test/test_geo_controller.c
+++ projects/lpc40xx_geo/l5_application/periodics/test/test_geo_controller.c
@@ -3,10 +3,67 @@
 #include "Mockgpio.h"
 #include "Mockgps.h"
 
+#include "haversine.h"
 #include "project.h"
 
-#include "geo_controller.h"
+#include "geo_controller.c"
 
 void setUp(void) {}
 
 void tearDown(void) {}
+
+void test_geo_controller__mia_happens() {
+  gpio_s gpio;
+  dest_coord.mia_info.mia_counter = 90;
+  board_io__get_led3_ExpectAndReturn(gpio);
+  gpio__reset_Expect(gpio);
+  geo_controller__manage_mia();
+}
+
+static void helper_function_to_encode_a_bridge_value(can__msg_t *msg, dbc_GPS_DESTINATION_s val) {
+  dbc_message_header_t header = dbc_encode_GPS_DESTINATION(msg->data.bytes, &val);
+  msg->frame_fields.data_len = header.message_dlc;
+  msg->msg_id = header.message_id;
+}
+
+void test_geo_controller__decode_bridge_message() {
+  can__msg_t msg;
+  dbc_GPS_DESTINATION_s val = {.GPS_DEST_LATITUDE_SCALED_100000 = 37 * 100000,
+                               .GPS_DEST_LONGITUDE_SCALED_100000 = -121 * 100000};
+  helper_function_to_encode_a_bridge_value(&msg, val);
+  geo_controller__decode_bridge_message(&msg);
+  TEST_ASSERT_EQUAL(val.GPS_DEST_LATITUDE_SCALED_100000, dest_coord.GPS_DEST_LATITUDE_SCALED_100000);
+  TEST_ASSERT_EQUAL(val.GPS_DEST_LONGITUDE_SCALED_100000, dest_coord.GPS_DEST_LONGITUDE_SCALED_100000);
+}
+
+void test_geo_controller__read_zero_messages() {
+  can__rx_ExpectAndReturn(can1, NULL, 0, false);
+  can__rx_IgnoreArg_can_message_ptr();
+  geo_controller__read_all_can_messages();
+}
+
+void test_geo_controller__read_one_messages() {
+  gpio_s gpio;
+  can__rx_ExpectAndReturn(can1, NULL, 0, true);
+  can__rx_IgnoreArg_can_message_ptr();
+  can__rx_ExpectAndReturn(can1, NULL, 0, false);
+  can__rx_IgnoreArg_can_message_ptr();
+  board_io__get_led3_ExpectAndReturn(gpio);
+  gpio__set_Expect(gpio);
+  geo_controller__read_all_can_messages();
+}
+
+void test_geo_controller__can_send_fail() {
+  can__tx_ExpectAndReturn(can1, NULL, 0, false);
+  can__tx_IgnoreArg_can_message_ptr();
+  TEST_ASSERT_FALSE(geo_controller__send_heading_to_driver_over_can());
+}
+
+void test_geo_controller__can_send_successful() {
+  gpio_s gpio;
+  can__tx_ExpectAndReturn(can1, NULL, 0, true);
+  can__tx_IgnoreArg_can_message_ptr();
+  gpio__toggle_Expect(gpio);
+  board_io__get_led0_ExpectAndReturn(gpio);
+  TEST_ASSERT_TRUE(geo_controller__send_heading_to_driver_over_can());
+}
