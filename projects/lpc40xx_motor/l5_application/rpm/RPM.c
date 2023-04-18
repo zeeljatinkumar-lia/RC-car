2b5cb826af332ad9d71d4f807731c59c3e765ae8
acmly <chj594696211@163.com>
2023-04-18 23:21:21 +0000

    Upload New File

---
+++ projects/lpc40xx_motor/l5_application/rpm/RPM.c
@@ -0,0 +1,45 @@
+#include "RPM.h"
+
+#include <stddef.h>
+
+#include "clock.h"
+#include "gpio.h"
+#include "lpc40xx.h"
+#include "lpc_peripherals.h"
+
+static const uint32_t timer_counter_upper_bound = 10000;
+
+static const float circumference_of_wheel_cm = 34.56f;
+static const float wheel_to_gear_ratio = 2.125f;
+static float kph_scalar = (circumference_of_wheel_cm * 3600) / (wheel_to_gear_ratio * 100000);
+static float speed_in_kph;
+
+static void timer2_isr(void) {
+  LPC_TIM2->TC = 0;
+  LPC_TIM2->IR |= 0x10;
+}
+
+void rpm_sensor__init(void) {
+  uint32_t prescalar_divider = 959;
+  lpc_peripheral__turn_on_power_to(LPC_PERIPHERAL__TIMER2);
+  LPC_TIM2->IR |= 0x3F;
+  LPC_TIM2->CCR = (1 << 0) | (1 << 2);
+  lpc_peripheral__enable_interrupt(LPC_PERIPHERAL__TIMER2, timer2_isr, NULL);
+  LPC_TIM2->PR = prescalar_divider;
+  LPC_TIM2->TCR |= (1 << 0);
+  gpio__construct_with_function(GPIO__PORT_2, 6, GPIO__FUNCTION_3);
+  kph_scalar *= clock__get_peripheral_clock_hz() / (prescalar_divider + 1);
+}
+
+void rpm_sensor__update_speed_value(void) {
+  uint32_t timer_capture_value = LPC_TIM2->CR0;
+  uint32_t timer_counter_value = LPC_TIM2->TC;
+  float offset = 58548.7f;
+  speed_in_kph = (kph_scalar - offset) / timer_capture_value;
+  if (timer_counter_value > timer_counter_upper_bound || timer_capture_value == 0) {
+    speed_in_kph = 0.0f;
+  }
+  printf("speed_in_kph %f kph_scalar=%f timer_capture_value=%d \n ", speed_in_kph, kph_scalar, timer_capture_value);
+}
+
+// float rpm_sensor__get_speed(void) { return speed_in_kph; }
